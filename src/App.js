fd86bb1294f38c8224491c6ced563b035940b4db
Klepnev <klepnev-ea@yandex.ru>
2017-11-10 19:15:08 +0300

    коммит

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React, {PureComponent} from 'react';
+
+let id = 0;
+
+function getNewsId () {
+  id += 1;
+  return id;  
+}
+
+function getCommentsId () {
+  id += 1;
+  return id;  
+}
+
+class App extends PureComponent {
+  state = {
+    news : [],
+    newsInput : ''
+  };
+
+    handleChange = (event) => {
+      const value = event.target.value;
+      this.setState({newsInput: value});
+    };
+
+    handleKeyDown = (event) => {
+      
+
+      if (event.keyCode === 13) {
+              const {newsInput, news} = this.state;
+              const newNews = {value: newsInput, id: getNewsId()};
+        
+              this.setState({newsInput: '', news: [ ...news, newNews]});
+      }
+
+    };
+
+  render() {
+    const {newsInput, news} = this.state;
+    return (
+      <div>
+        <input 
+          value={newsInput}
+          onChange={this.handleChange} 
+          onKeyDown={this.handleKeyDown}
+        />
+        <div className="App">
+          {news.map(news => <News id={news.id} key={news.id} text = {news.value} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class News extends PureComponent {
+  state = {
+    id: 0,
+    newsComments : '',
+    comments: []
+  };
+
+  handleChange = (event) => {
+    const value = event.target.value;
+    this.setState({newsComments: value});
+  };
+
+  handleKeyDown = (event) => {
+    if (event.keyCode === 13) {
+            const {newsComments, comments} = this.state;
+            const myComments = {value: newsComments, id: getCommentsId()};
+      
+            this.setState({newsComments: '', comments: [ ...comments, myComments]});
+    }
+  };
+
+  handleDelete = (id) => {
+    this.setState(state => ({
+      comments: state.comments.filter(comment => id != comment.id)
+    }));
+  };
+
+  render() {
+    const {text} = this.props; 
+    const {newsComments, comments} = this.state;
+    return ( 
+      <div>
+        <p className="news">{text}</p> 
+        <div className="comments-wrap">
+          {comments.map(comments => (<Comments onDelete={this.handleDelete} id={comments.id} key={comments.id} text = {comments.value} />))}
+        </div>
+          
+        <input 
+          value={newsComments}
+          onChange={this.handleChange} 
+          onKeyDown={this.handleKeyDown}
+        />
+      </div>
+    );
+  }
+}
+
+class Comments extends PureComponent {
+
+  handleDelete = () => {
+    const {id, onDelete} = this.props;
+    onDelete(id);
+  }
+
+  render() {
+    const {text} = this.props;  
+    return (<p className="comments">{text} <span onClick={this.handleDelete} className="delete">X</span></p>   );
+  }
+}
+
+
+export default App;
